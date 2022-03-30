7783c3c382c9f9459a948bdb70c3b2826159194e
Xiejiadong <1934784736@qq.com>
2022-03-30 17:25:18 +0800

    Prepare for experiments.

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -17,150 +17,214 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <map>
+#include <fstream>
 
 #include "milvus/MilvusClient.h"
 #include "milvus/types/CollectionSchema.h"
 
-void
-CheckStatus(std::string&& prefix, const milvus::Status& status) {
+const int topk = 50;
+const int nlist = 1024;
+const int search_limit = 512;
+const int SIZE_COLLECTION = 46201;
+const int SIZE_QUERY = 5133;
+const std::string img_collection_name = "recipe_img_normalized";
+const std::string ins_collection_name = "recipe_instr_normalized";
+std::string outputResult = "/result/";
+int m = 1024;
+
+
+std::map<float,float> RESULT;
+
+void CheckStatus(std::string&& prefix, const milvus::Status& status) {
     if (!status.IsOk()) {
         std::cout << prefix << " " << status.Message() << std::endl;
         exit(1);
     }
 }
 
-int
-main(int argc, char* argv[]) {
-    printf("Example start...\n");
+bool Search_topk(int TOPK, std::vector<float> img_target_vector, std::vector<float> ins_target_vector) {
+    
+}
 
-    auto client = milvus::MilvusClient::Create();
+int main(int argc, char* argv[]) {
+    std::string img_filename = "/embeddings/img_embeds_query.tsv";
+    std::string ins_filename = "/embeddings/rec_embeds_query.tsv";
+    printf("Experiments start...\n");
+    
+    auto img_client = milvus::MilvusClient::Create();
+    auto ins_client = milvus::MilvusClient::Create();
 
     milvus::ConnectParam connect_param{"localhost", 19530};
-    auto status = client->Connect(connect_param);
+    auto status = img_client->Connect(connect_param);
+    CheckStatus("Failed to connect milvus server:", status);
+    status = ins_client->Connect(connect_param);
     CheckStatus("Failed to connect milvus server:", status);
     std::cout << "Connect to milvus server." << std::endl;
 
-    // drop the collection if it exists
-    const std::string collection_name = "TEST";
-    status = client->DropCollection(collection_name);
 
-    // create a collection
-    const std::string field_id_name = "identity";
-    const std::string field_age_name = "age";
-    const std::string field_face_name = "face";
-    const uint32_t dimension = 4;
-    milvus::CollectionSchema collection_schema(collection_name);
-    collection_schema.AddField({field_id_name, milvus::DataType::INT64, "user id", true, false});
-    collection_schema.AddField({field_age_name, milvus::DataType::INT8, "user age"});
-    collection_schema.AddField(milvus::FieldSchema(field_face_name, milvus::DataType::FLOAT_VECTOR, "face signature")
-                                   .WithDimension(dimension));
-
-    status = client->CreateCollection(collection_schema);
-    CheckStatus("Failed to create collection:", status);
-    std::cout << "Successfully create collection." << std::endl;
-
-    // create a partition
-    std::string partition_name = "Year_2022";
-    status = client->CreatePartition(collection_name, partition_name);
-    CheckStatus("Failed to create partition:", status);
-    std::cout << "Successfully create partition." << std::endl;
-
-    // tell server prepare to load collection
-    status = client->LoadCollection(collection_name);
+    status = img_client->LoadCollection(img_collection_name);
     CheckStatus("Failed to load collection:", status);
+    status = ins_client->LoadCollection(ins_collection_name);
+    CheckStatus("Failed to load collection:", status);
+    
+	std::ifstream img_in(img_filename.c_str());
+    if (!img_in)
+	{
+		std::cout << "Fail to read img query " << std::endl;
+		return 0;
+	}
+    std::ifstream ins_in(ins_filename.c_str());
+    if (!img_in)
+	{
+		std::cout << "Fail to read ins query " << std::endl;
+		return 0;
+	}
+    std::vector<float> img_query_id, ins_query_id;
+    std::vector<std::vector<float> > img_query, ins_query;
+    img_query.resize(SIZE_QUERY);
+    ins_query.resize(SIZE_QUERY);
+    std::string line;
+    int num = 0;
+    for (; getline(img_in, line);)
+	{
+        img_query_id.push_back(std::stof(line.substr(0, line.find("\t"))));
 
-    // insert some rows
-    const int64_t row_count = 1000;
-    std::vector<int64_t> insert_ids;
-    std::vector<int8_t> insert_ages;
-    std::vector<std::vector<float>> insert_vectors;
-    std::default_random_engine ran(time(nullptr));
-    std::uniform_int_distribution<int8_t> int_gen(1, 100);
-    std::uniform_real_distribution<float> float_gen(0.0, 1.0);
-    for (auto i = 0; i < row_count; ++i) {
-        insert_ids.push_back(i);
-        insert_ages.push_back(int_gen(ran));
-        std::vector<float> vector(dimension);
-
-        for (auto i = 0; i < dimension; ++i) {
-            vector[i] = float_gen(ran);
+		line.erase(0, line.find("[") + 1);
+        for (int j = 0; j < m - 1; j++) {
+            img_query[num].push_back(std::stof(line.substr(0, line.find(","))));
+            line.erase(0, line.find(",") + 2);
         }
-        insert_vectors.emplace_back(vector);
+        img_query[num].push_back(std::stof(line.substr(0, line.find("]"))));
+        //normalization;
+        double sum = 0.0;
+        for (auto i = 0; i < img_query[num].size(); i++)
+            sum += img_query[num][i] * img_query[num][i];
+        sum = sqrt(sum);
+        for (auto i = 0; i < img_query[num].size(); i++)
+            img_query[num][i] /= sum;
+        num++;
+    }
+    num = 0;
+    for (; getline(ins_in, line);)
+	{
+        ins_query_id.push_back(std::stof(line.substr(0, line.find("\t"))));
+
+		line.erase(0, line.find("[") + 1);
+        for (int j = 0; j < m - 1; j++) {
+            ins_query[num].push_back(std::stof(line.substr(0, line.find(","))));
+            line.erase(0, line.find(",") + 2);
+        }
+        ins_query[num].push_back(std::stof(line.substr(0, line.find("]"))));
+        //normalization;
+        double sum = 0.0;
+        for (auto i = 0; i < ins_query[num].size(); i++)
+            sum += ins_query[num][i] * ins_query[num][i];
+        sum = sqrt(sum);
+        for (auto i = 0; i < ins_query[num].size(); i++)
+            ins_query[num][i] /= sum;
+        num++;
     }
 
-    std::vector<milvus::FieldDataPtr> fields_data{
-        std::make_shared<milvus::Int64FieldData>(field_id_name, insert_ids),
-        std::make_shared<milvus::Int8FieldData>(field_age_name, insert_ages),
-        std::make_shared<milvus::FloatVecFieldData>(field_face_name, insert_vectors)};
-    milvus::DmlResults dml_results;
-    status = client->Insert(collection_name, partition_name, fields_data, dml_results);
-    CheckStatus("Failed to insert:", status);
-    std::cout << "Successfully insert " << dml_results.IdArray().IntIDArray().size() << " rows." << std::endl;
+    std::string output_result_path = outputResult + "qrels.txt";
+    std::string output_lantency_path = outputResult + "latency.txt";
+    std::ofstream out1(output_result_path.c_str());
+    if (!out1.is_open())
+    {
+        std::cout << "Cannot open file out1" << std::endl;
+        return 0;
+    }
+    std::ofstream out2(output_lantency_path.c_str());
+    if (!out2.is_open())
+    {
+        std::cout << "Cannot open file out2" << std::endl;
+        return 0;
+    }
 
-    // get partition statistics
-    milvus::PartitionStat part_stat;
-    status = client->GetPartitionStatistics(collection_name, partition_name, part_stat);
-    CheckStatus("Failed to get partition statistics:", status);
-    std::cout << "Partition " << partition_name << " row count: " << part_stat.RowCount() << std::endl;
+    for (int i = 0; i <= SIZE_QUERY; i++)
+    {
+        double Begin_time = clock();
+        int limit = 1;
+        while(limit < topk) limit *= 2;
+        while(limit <= search_limit && limit <= SIZE_COLLECTION) {
+            bool flag;
+            int TOPK = limit;
+            int nprobe = TOPK / (SIZE_COLLECTION / nlist);
+            
+            milvus::SearchArguments img_arguments{};
+            img_arguments.SetCollectionName(img_collection_name);
+            img_arguments.SetTopK(TOPK);
+            img_arguments.SetGuaranteeTimestamp(milvus::GuaranteeStrongTs());
+            img_arguments.AddTargetVector("img_embeds", img_query[i]);
+            img_arguments.AddExtraParam("nprobe", nprobe);
+            milvus::SearchResults img_search_results{};
+            auto status = img_client->Search(img_arguments, img_search_results);
+            CheckStatus("Failed to search:", status);
+            std::cout << "img Successfully search." << std::endl;
 
-    // do search
-    milvus::SearchArguments arguments{};
-    arguments.SetCollectionName(collection_name);
-    arguments.AddPartitionName(partition_name);
-    arguments.SetTopK(10);
-    arguments.AddOutputField(field_age_name);
-    arguments.SetExpression(field_age_name + " > 40");
-    // set to strong guarantee so that the search is executed after the inserted data is persisted
-    arguments.SetGuaranteeTimestamp(milvus::GuaranteeStrongTs());
+            milvus::SearchArguments ins_arguments{};
+            ins_arguments.SetCollectionName(ins_collection_name);
+            ins_arguments.SetTopK(TOPK);
+            ins_arguments.SetGuaranteeTimestamp(milvus::GuaranteeStrongTs());
+            ins_arguments.AddTargetVector("rec_embeds", ins_query[i]);
+            ins_arguments.AddExtraParam("nprobe", nprobe);
+            milvus::SearchResults ins_search_results{};
+            status = ins_client->Search(ins_arguments, ins_search_results);
+            CheckStatus("Failed to search:", status);
+            std::cout << "ins Successfully search." << std::endl;
 
-    std::uniform_int_distribution<int64_t> int64_gen(0, row_count - 1);
-    int64_t q_number = int64_gen(ran);
-    std::vector<float> q_vector = insert_vectors[q_number];
-    arguments.AddTargetVector(field_face_name, std::move(q_vector));
-    std::cout << "Searching the No." << q_number << " entity..." << std::endl;
+            for (auto& img_result : img_search_results.Results()) {
+                auto& img_ids = img_result.Ids().IntIDArray();
+                auto& img_distances = img_result.Scores();
+                if (img_ids.size() != img_distances.size()) {
+                    std::cout << "img Illegal result!" << std::endl;
+                    continue;
+                }
+                for (auto& ins_result : ins_search_results.Results()) {
+                    auto& ins_ids = ins_result.Ids().IntIDArray();
+                    auto& ins_distances = ins_result.Scores();
+                    if (ins_ids.size() != ins_distances.size()) {
+                        std::cout << "img Illegal result!" << std::endl;
+                        continue;
+                    }
 
-    milvus::SearchResults search_results{};
-    status = client->Search(arguments, search_results);
-    CheckStatus("Failed to search:", status);
-    std::cout << "Successfully search." << std::endl;
-
-    for (auto& result : search_results.Results()) {
-        auto& ids = result.Ids().IntIDArray();
-        auto& distances = result.Scores();
-        if (ids.size() != distances.size()) {
-            std::cout << "Illegal result!" << std::endl;
-            continue;
-        }
-
-        auto age_field = result.OutputField(field_age_name);
-        milvus::Int8FieldDataPtr age_field_ptr = std::static_pointer_cast<milvus::Int8FieldData>(age_field);
-        auto& age_data = age_field_ptr->Data();
-
-        for (size_t i = 0; i < ids.size(); ++i) {
-            std::cout << "ID: " << ids[i] << "\tDistance: " << distances[i]
-                      << "\tAge: " << static_cast<int32_t>(age_data[i]) << std::endl;
-            // validate the age value
-            if (insert_ages[ids[i]] != age_data[i]) {
-                std::cout << "ERROR! The returned value doesn't match the inserted value" << std::endl;
+                    std::unordered_map<float,float> merge_score;
+                    RESULT.clear();
+                    for (int i = 0; i < ins_ids.size(); i++){
+                        if (merge_score.find(img_ids[i]) == merge_score.end())
+                            merge_score[img_ids[i]] = img_distances[i];
+                        else RESULT[img_ids[i]] = merge_score[img_ids[i]] + img_distances[i];
+                        if (merge_score.find(ins_ids[i]) == merge_score.end())
+                            merge_score[ins_ids[i]] = ins_distances[i];
+                        else RESULT[ins_ids[i]] = merge_score[ins_ids[i]] + ins_distances[i];
+                        if (RESULT.size() > topk)
+                        {
+                            auto tmp = RESULT.end();
+                            tmp--;
+                            RESULT.erase(tmp);
+                        }
+                    }
+                    if (RESULT.size() == topk) flag = true;
+                    flag = false;
+                    
+                }
             }
+
+
+
+
+            if (flag) {
+                double End_time = clock();
+                for (auto x : RESULT)
+                    out1 << img_query_id[i] << x.first << x.second << std::endl;
+                out2 << img_query_id[i] << End_time - Begin_time << std::endl;
+                break;
+            }
+            limit *= 2;
         }
+        int number = i + 1;
+        if (number % 5 == 0)
+            std::cout << number << "queries searched." << std::endl;
     }
-
-    // drop partition
-    status = client->DropPartition(collection_name, partition_name);
-    CheckStatus("Failed to drop partition:", status);
-    std::cout << "Drop partition " << partition_name << std::endl;
-
-    // verify the row count should be 0
-    milvus::CollectionStat col_stat;
-    status = client->GetCollectionStatistics(collection_name, col_stat);
-    CheckStatus("Failed to get collection statistics:", status);
-    std::cout << "Collection " << collection_name << " row count: " << col_stat.RowCount() << std::endl;
-
-    // drop collection
-    status = client->DropCollection(collection_name);
-    CheckStatus("Failed to drop collection:", status);
-    std::cout << "Drop collection " << collection_name << std::endl;
-
     return 0;
 }
